d2c84dd3983ff9f50fd7c7e8c2fbd577989ee31c
Yauheniya Dubovik <dubvik.evgesha@gmail.com>
2020-01-21 11:29:39 +0300

    Comint

---
+++ Task2ArrayValues/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace Task2ArrayValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] {28, 66, 81, 91, 55, 125, 11, 369, 13, 21};
+            Array.Sort(arr);
+            Console.WriteLine($"Two Min values in array are {arr[0]} ; {arr[1]}");
+
+        }
+    }
+}
